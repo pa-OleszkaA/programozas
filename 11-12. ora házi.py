b2d4ad3d3ed44bad7bf923ff7ccc873d66d327f0
pa_oleszkaa <112852315+pa-OleszkaA@users.noreply.github.com>
2022-10-11 14:00:22 +0200

    Update 11-12. ora házi.py

--- 11-12. ora házi.py
+++ 11-12. ora házi.py
@@ -1,10 +1,10 @@
 egyikszam=int(input("Kérem az elsö számot 1-00-ig"))
 masikszam=int(input("Kérem az masik számot 1-00-ig"))
-if egyikszam==masikszam:
-    print('megegyezik')
+if egyikszam<masikszam:
+    print("A masodik szám kisebb, a különbségük", egyikszam-masikszam)
 elif  egyikszam>masikszam:
-    print("az első szám nagyobb" , egyikszam-masikszam, "nagyobb")
-    print("ennyivel:",egyikszam-masikszam)
+    print("A masodik szám kisebb, a különbségük", egyikszam-masikszam)
+    
 else:
     print("megegyezik")
         
