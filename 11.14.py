8773b2d63039189488312b28b5bf7dd482e3a3ea
pa_oleszkaa <112852315+pa-OleszkaA@users.noreply.github.com>
2022-11-14 08:53:49 +0100

    Create 11.14.py

---
+++ 11.14.py
@@ -0,0 +1,54 @@
+"""
+a = "Hello, World!"
+
+print(a.upper())
+
+a = "Hello, World!"
+
+print(a.lower())
+
+print(a.strip()) # returns "Hello World!"
+
+a = "Hello,World! Hello,Hello,"
+print(a)
+uj_string =print(a.replace("Hello,World!,Hello,Hello", "xxx"))
+print("a régi sztring" +" " + a)
+print("a régi sztring",a, sep = " ")
+print("az uj " + uj_string)
+
+"""
+"""
+a = "Hello"
+b = "World"
+d = " "
+c = a + d + b
+print(c)
+
+a = "Hello"
+b = "World"
+space = " "
+felkialtojel = " ! "
+c = a + space + b + felkialtojel + space + a + space+ b + felkialtojel
+print (c)
+
+"""
+"""
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = " I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(5+6, itemno, price))
+
+"""
+"""
+print("10.D osztály 1. csoport", end='\n')
+print("10.D osztály 1. csoport", end='\t\t\t ')
+print("10.D osztály 1. csoport")
+
+"""
+b= "Hello, World!"
+
+print(b[-6:-1]) # orl
+print(b[-5:]) #orld!
+print
+
