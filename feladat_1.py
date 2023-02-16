4856627dc92de3f708a793c238888ceb36f5de91
pa_oleszkaa <112852315+pa-OleszkaA@users.noreply.github.com>
2023-02-16 07:52:00 +0100

    Add files via upload

---
+++ feladat_1.py
@@ -0,0 +1,11 @@
+# Oleszka Attila
+#10.D 1. csoport
+
+szam = int(input("Adjon meg egy számot! "))
+if szam < 0:
+    print("A megadott szám negatív. ")
+elif szam == 0:
+    print("A megadott szám nulla. ")
+else:
+    print("A megadott szám pozitív. ")
+
