4db107f2e1882db21fd657eaaa5ef5bca8eb3345
pa_oleszkaa <112852315+pa-OleszkaA@users.noreply.github.com>
2023-02-23 07:42:19 +0100

    Add files via upload

---
+++ feladat_2.py
@@ -0,0 +1,7 @@
+#Oleszka Attila
+#10.D 1. csoport 2. feladat
+
+szo = None
+while szo != '':
+      szo = input('Adj meg szavakat! Ha kilépnél, a szó helyett csak egy ENTER-t üss! ')    
+print('Program vége!')
